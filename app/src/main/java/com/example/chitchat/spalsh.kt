d5a091f48917112e725ef7fa63718603db9f63b4
Ayush Luthra <ayushluthra62@gmail.com>
2023-02-17 22:53:03 +0530

    Added spalsh Screen or Ui Improved

---
+++ app/src/main/java/com/example/chitchat/spalsh.kt
@@ -0,0 +1,21 @@
+package com.example.chitchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class spalsh : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalsh)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent=Intent(this,Login::class.java)
+            startActivity(intent)
+            finish()
+        },1500)
+
+    }
+}
