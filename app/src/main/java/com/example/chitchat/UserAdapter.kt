708751671e85f78f99c1949692ec7de6394e60b9
Ayush Luthra <ayushluthra62@gmail.com>
2023-02-20 00:07:08 +0530

    Recycler View Added

---
+++ app/src/main/java/com/example/chitchat/UserAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.chitchat
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class UserAdapter :RecyclerView.Adapter {
+    class userViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
